4ecf73c7084f3f9d047b3c83914187cc17c2069c
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-06-10 16:28:20 -0500

    Added my_each_with_index

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 module Enumerable
   def my_each
     return to_enum unless block_given?
-
     i = 0
     while i < length
       yield(self[i])
@@ -9,9 +8,31 @@ module Enumerable
     end
     self
   end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+    i = 0
+    while i < length
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
 end
 
 # Testing comparison
+# each test
 a = %w[a b c]
-a.each { |x| print x, ' -- ' }
-a.my_each { |x| print x, ' -- ' }
+a.each { |x| p x, ' -- ' }
+a.my_each { |x| p x, ' -- ' }
+# each_with_index test
+hash = Hash.new
+my_hash = Hash.new
+%w(cat dog wombat).each_with_index { |item, index|
+  hash[item] = index
+}
+%w(cat dog wombat).my_each_with_index { |item, index|
+  my_hash["my " + item] = index
+}
+p hash
+p my_hash
