316ab62e3952f0de238ce9f03a492cf1e037b4be
Abel <abel.lavieri@gmail.com>
2020-06-11 12:07:42 -0400

    Added my none method

--- main.rb
+++ main.rb
@@ -49,4 +49,18 @@ module Enumerable
             cond
         end
     end
+
+    def my_none?(arg = nil)
+        cond = true
+        if block_given?
+            self.my_each{|el| cond = cond && yield(el)}
+            !cond
+        elsif arg
+            self.my_each{|el| cond = cond && arg===el}
+            !cond
+        else
+            self.my_each{|el| cond = cond && true === el}
+            !cond
+        end
+    end
 end
