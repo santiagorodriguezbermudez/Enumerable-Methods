d3e6322ff93f66f5461822c9054dbfd95813fd6f
Abel <abel.lavieri@gmail.com>
2020-06-11 17:17:34 -0400

    Fixed most of the rubocop errors

--- main.rb
+++ main.rb
@@ -1,77 +1,66 @@
-if block_given?
-end
 module Enumerable
   def my_each
-    return to_enum unless block_given?  
-    each do |elem|
+    return to_enum unless block_given?
+
+    for elem in self
       yield(elem)
     end
     self
   end
 
   def my_each_with_index
-    if block_given?
-      index = 0
-      each do |elem|
-        yield(elem, index)
-        index += 1
-      end
-      self
-    else
-      to_enum
+    return to_enum unless block_given?
+
+    index = 0
+    for elem in self
+      yield(elem, index)
+      index += 1
     end
+    self
   end
 
   def my_select
-    if block_given?
-      new_arr = []
-      my_each { |el| new_arr.push(el) if yield(el) }
-      new_arr
-    else
-      to_enum
-    end
+    return to_enum unless block_given?
+
+    new_arr = []
+    my_each { |el| new_arr.push(el) if yield(el) }
+    new_arr
   end
 
   def my_all?(arg = nil)
     cond = true
     if block_given?
       my_each { |el| cond &&= yield(el) }
-      cond
     elsif arg
       my_each { |el| cond &&= arg === el }
-      cond
     else
       my_each { |el| cond &&= true === el }
-      cond
     end
+    cond
   end
 
   def my_none?(arg = nil)
     cond = true
     if block_given?
       my_each { |el| cond &&= yield(el) }
-      !cond
     elsif arg
       my_each { |el| cond &&= arg === el }
-      !cond
     else
       my_each { |el| cond &&= true === el }
-      !cond
     end
+    !cond
   end
 
   def my_any?(arg = nil)
     cond = false
     if block_given?
       my_each { |el| cond ||= yield(el) }
-      cond
     elsif arg
       my_each { |el| cond ||= arg === el }
-      cond
     else
       my_each { |el| cond ||= true === el }
-      cond
     end
+    cond
   end
 
   def my_count(arg = nil)
@@ -96,29 +85,28 @@ module Enumerable
   end
 
   def my_inject(*args)
-    if block_given?
-      memo = args[0] || ((first.is_a? Numeric) ? 0 : first)
-      my_each { |el| memo = yield(memo, el) }
-      memo
-    elsif args.length == 0
-      raise(LocalJumpError.new, 'No block or argument given')
+    if args && !args[0].is_a? Symbol
+      memo=args[0]
+    elsif first.is_a? Numeric
+      memo=0
     else
-      case args.length
-      when 1
-        if args[0].is_a? Symbol
-          memo = ((first.is_a? Numeric) ? 0 : first)
-          my_each { |el| memo = memo&.send(args[0], el) }
-          memo
-        end
-      when 2
-        memo = args[0]
-        my_each { |el| memo = memo&.send(args[1], el) }
-        memo
-      end
+      memo=first
     end
+    if block_given?
+      my_each { |el| memo = yield(memo, el) }
+    elsif args.empty?
+      raise(LocalJumpError.new, 'No block or argument given')
+    elsif args.length == 1
+      my_each { |el| memo = memo.send(args[0], el) }
+    else
+      my_each { |el| memo = memo.send(args[1], el) }
+    end
+    memo
   end
 end
 
 def multiply_els(arr)
   arr.my_inject(1, :*)
 end
+
+p (5..10).my_inject(:+)
