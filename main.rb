46e9fbc2a17bd4b4331838abca102cd38f1c68c6
Abel <abel.lavieri@gmail.com>
2020-06-11 12:02:53 -0400

    Added each, each w/ index, select and all methods

--- main.rb
+++ main.rb
@@ -1,3 +1,52 @@
-def Enumerable:
-  
+if block_given?
+else
+end
+module Enumerable
+    def my_each
+        if block_given?
+            for elem in self
+                yield(elem)
+            end
+            self
+        else
+            self.to_enum
+        end
+    end
+
+    def my_each_with_index
+        if block_given?
+            index=0
+            for elem in self
+                yield(elem,index)
+                index += 1
+            end
+            self
+        else
+            self.to_enum
+        end
+    end
+
+    def my_select
+        if block_given?
+            new_arr = []
+            self.my_each {|el| new_arr.push(el) if yield(el)}
+            new_arr
+        else
+            self.to_enum
+        end
+    end
+
+    def my_all?(arg = nil)
+        cond = true
+        if block_given?
+            self.my_each{|el| cond = cond && yield(el)}
+            cond
+        elsif arg
+            self.my_each{|el| cond = cond && arg===el}
+            cond
+        else
+            self.my_each{|el| cond = cond && true === el}
+            cond
+        end
+    end
 end
